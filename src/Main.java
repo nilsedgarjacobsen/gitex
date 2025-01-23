a40933f86274764246e4acf41edf80103d075988
nilsedgarjacobsen <nils.jacobsen@zocom.se>
2025-01-23 11:59:57 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        //Hej detta har jag gjort på github alltså mitt remote repo
     }
 }
